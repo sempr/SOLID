78f95dcca2de574454e7fcf6b48ff708024dd6e2
Sempr <iamsempr@gmail.com>
2018-09-22 09:33:15 +0800

    Perimeter

--- python_code/good/open_close.py
+++ python_code/good/open_close.py
@@ -8,12 +8,17 @@
 import math
 from abc import ABCMeta, abstractproperty
 
+
 class Shape(object):
     __metaclass__ = ABCMeta
 
     @abstractproperty
     def area(self):
-        pass
+        """"""
+
+    @abstractproperty
+    def perimeter(self):
+        """"""
 
 class Rectangle(Shape):
 
@@ -25,6 +30,10 @@ class Rectangle(Shape):
     def area(self):
         return self.width * self.height
 
+    @property
+    def perimeter(self):
+        """"""
+        return (self.width + self.height)*2
 
 class Square(Shape):
     def __init__(self, side_length):
@@ -34,6 +43,10 @@ class Square(Shape):
     def area(self):
         return self.side_length ** 2
 
+    @property
+    def perimeter(self):
+        """"""
+        return self.side_length * 4
 
 class Circle(Shape):
     def __init__(self, radius):
@@ -42,6 +55,11 @@ class Circle(Shape):
     @property
     def area(self):
         return self.radius ** 2 * math.pi
+
+    @property
+    def perimeter(self):
+        """"""
+        return math.pi * self.radius * 2
 
 class AreaCalculator(object):
 
@@ -55,6 +73,12 @@ class AreaCalculator(object):
             total += shape.area
         return total
 
+    @property
+    def total_perimeter(self):
+        total = 0
+        for shape in self.shapes:
+            total += shape.perimeter
+        return total
 # Note that if we want to extend the functionality of AreaCalculator to support calculating
 # area of different shape, we only need to define new subtype of `Shape` and leave other code
 # alone without modify them. That is the key of open/close principle.
@@ -64,6 +88,7 @@ def main():
     calculator = AreaCalculator(shapes)
 
     print "The total area is: ", calculator.total_area
+    print "The total perimeter is: ", calculator.total_perimeter
 
 if __name__ == '__main__':
     main()
